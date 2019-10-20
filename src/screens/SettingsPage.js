54232b1a749cceae604042ff8cb387b5c8ffd639
johnjiang2017 <johnhappen@hotmail.com>
2019-10-19 21:23:03 -0400

    added redux

--- src/screens/SettingsPage.js
+++ src/screens/SettingsPage.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import UserInfo from '../components/UserInfo'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack'
+
+import SettingDetails from '../components/SettingDetails'
+
+const RouteConfigs = {
+    settingDetail : {
+        screen: SettingDetails,
+    },
+    userInfo: {
+        screen: UserInfo
+    }
+}
+
+const StackNavigatorConfig = ({ navigation }) => ({
+    title: 'Settings',
+    defaultNavigationOptions : settingDetail
+  })
+
+const pageStack = createStackNavigator(RouteConfigs, StackNavigatorConfig);
+
+const SettingPage = createAppContainer(pageStack)
 
 export default class SettingsPage extends React.Component{
     render(){
-        return(
-            <View style={styles.container}>
-                <Text style={styles.homeText}>Settings Page</Text>
-                <UserInfo />
-            </View>
-        )
+        return <SettingPage/>
     }
 }
 
