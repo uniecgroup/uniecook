775ae47613d846c87129d994d3956baf1e4a335c
uniecgroup <uniecgroup@gmail.com>
2019-10-25 10:09:27 -0400

    add StyleSheet

--- src/navigation/NewOrderNavigator.js
+++ src/navigation/NewOrderNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import NewOrderListPage from '../screens/NewOrderListPage';
